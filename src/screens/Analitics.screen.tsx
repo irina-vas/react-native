6f02a286598c48de13ffa33aba3d2f7f88f1a6dd
iryna vasylenko <irina.v@twsgo.com>
2022-07-13 17:21:45 +0100

    added analitics section

--- src/screens/Analitics.screen.tsx
+++ src/screens/Analitics.screen.tsx
@@ -7,9 +7,38 @@ import { theme } from '../theme';
 
 export const Analitics: FC = () => {
   const appContext = useAppContext();
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    })
+  );
   return (
-    <View>
-      <Text>Analitics</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Analitics</Text>
+      <VictoryPie
+        data={data}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontFamily: theme.fontFamilyBold,
+    marginBottom: 30,
+  }
+})
